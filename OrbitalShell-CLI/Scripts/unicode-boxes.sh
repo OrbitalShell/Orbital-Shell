9c956d0ab23849c44759db5a4614f11240f2f3ea
franck gaspoz <franck.gaspoz@gmail.com>
2021-01-20 01:06:40 +0100

    added Scripts folder in -CLI. scripts that will be found in .SHELL/Scripts

---
+++ OrbitalShell-CLI/Scripts/unicode-boxes.sh
@@ -0,0 +1,8 @@
+#!orbsh
+# draw boxes using unicodes characters (fgz - 19/1/11)
+echo "(EdgeTopLeft,EdgeTopRight)1 (2 lines box)(br)(EdgeBottomLeft,EdgeBottomRight)"
+echo "(EdgeTopLeft,BarHorizontal,BarHorizontal,BarHorizontal,EdgeTopRight)1 (3 lines box)(br)(BarVertical) ? (BarVertical)2(br)(EdgeBottomLeft,BarHorizontal,BarHorizontal,BarHorizontal,EdgeBottomRight)3"
+
+
+echo "(EdgeTopLeft,EdgeTopRight)(br)(EdgeBottomLeft,EdgeBottomRight)"
+echo "(EdgeTopLeft,BarHorizontal,BarHorizontal,BarHorizontal,EdgeTopRight)(br)(BarVertical) ? (BarVertical)(br)(EdgeBottomLeft,BarHorizontal,BarHorizontal,BarHorizontal,EdgeBottomRight)"
