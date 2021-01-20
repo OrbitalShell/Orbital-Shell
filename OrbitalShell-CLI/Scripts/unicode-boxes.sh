04dfef78a709c0bb936d673001384d2df20fd68a
franck gaspoz <franck.gaspoz@gmail.com>
2021-01-20 01:27:29 +0100

    improved script unicode boxes

--- OrbitalShell-CLI/Scripts/unicode-boxes.sh
+++ OrbitalShell-CLI/Scripts/unicode-boxes.sh
@@ -3,6 +3,11 @@
 echo "(EdgeTopLeft,EdgeTopRight)1 (2 lines box)(br)(EdgeBottomLeft,EdgeBottomRight)"
 echo "(EdgeTopLeft,BarHorizontal,BarHorizontal,BarHorizontal,EdgeTopRight)1 (3 lines box)(br)(BarVertical) ? (BarVertical)2(br)(EdgeBottomLeft,BarHorizontal,BarHorizontal,BarHorizontal,EdgeBottomRight)3"
 
-
 echo "(EdgeTopLeft,EdgeTopRight)(br)(EdgeBottomLeft,EdgeBottomRight)"
 echo "(EdgeTopLeft,BarHorizontal,BarHorizontal,BarHorizontal,EdgeTopRight)(br)(BarVertical) ? (BarVertical)(br)(EdgeBottomLeft,BarHorizontal,BarHorizontal,BarHorizontal,EdgeBottomRight)"
+
+echo "(EdgeDoubleTopLeft,EdgeDoubleTopRight)1 (2 lines box)(br)(EdgeDoubleBottomLeft,EdgeDoubleBottomRight)"
+echo "(EdgeDoubleTopLeft,BarDoubleHorizontal,BarDoubleHorizontal,BarDoubleHorizontal,EdgeDoubleTopRight)1 (3 lines box)(br)(BarDoubleVertical) ? (BarDoubleVertical)2(br)(EdgeDoubleBottomLeft,BarDoubleHorizontal,BarDoubleHorizontal,BarDoubleHorizontal,EdgeDoubleBottomRight)3"
+
+echo "(EdgeDoubleTopLeft,EdgeDoubleTopRight)(br)(EdgeDoubleBottomLeft,EdgeDoubleBottomRight)"
+echo "(EdgeDoubleTopLeft,BarHorizontal,BarDoubleHorizontal,BarDoubleHorizontal,EdgeDoubleTopRight)(br)(BarDoubleVertical) ? (BarDoubleVertical)(br)(EdgeDoubleBottomLeft,BarDoubleHorizontal,BarDoubleHorizontal,BarDoubleHorizontal,EdgeDoubleBottomRight)"
