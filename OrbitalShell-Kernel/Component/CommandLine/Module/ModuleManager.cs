28db6a858edc678b9c3b68969a0212b4516d868c
franck gaspoz <franck.gaspoz@gmail.com>
2021-01-26 15:31:46 +0100

    set all projects to netcore3.1 (remove standard2.1 coz vscode omnisharp intellisense fail on it) - net5.0 is too slow (console output default)

---
+++ OrbitalShell-Kernel/Component/CommandLine/Module/ModuleManager.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace OrbitalShell.Component.CommandLine.Module
+{
+    /// <summary>
+    /// manage the shell modules
+    /// </summary>
+    public class ModuleManager
+    {
+
+    }
+}
