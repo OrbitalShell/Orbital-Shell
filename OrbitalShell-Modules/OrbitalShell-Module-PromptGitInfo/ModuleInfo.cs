b58e3ff21d78ad661ebf5811afc8ce229917e6fa
franck gaspoz <franck.gaspoz@gmail.com>
2021-02-09 22:19:59 +0100

    inc version git-prompt-info

--- OrbitalShell-Modules/OrbitalShell-Module-PromptGitInfo/ModuleInfo.cs
+++ OrbitalShell-Modules/OrbitalShell-Module-PromptGitInfo/ModuleInfo.cs
@@ -7,7 +7,6 @@ using OrbitalShell.Lib;
 [assembly: ShellModule()]
 [assembly: ModuleTargetPlateform(TargetPlatform.Any)]
 [assembly: ModuleShellMinVersion("1.0.0-beta-4")]
-[assembly: ModuleDependency("OrbitalShell-Kernel", "1.0.0-beta-4")]
 [assembly: ModuleAuthors("Orbital Shell team")]
 namespace OrbitalShell.Module.PromptGitInfo
 {
