659dcdbbdccc368b186bb70bd1bb487f6d8b4532
franck gaspoz <franck.gaspoz@gmail.com>
2021-02-15 22:31:16 +0100

    fix csproj and packages spec

---
+++ OrbitalShell-CLI/Scripts/com-dev-notes.sh
@@ -0,0 +1,36 @@
+﻿#!orbsh
+# commands - notes
+# ------------------------------------------------------------------------------------------------
+
+# list orbital shell nupkg packages files (after nuget install)
+# -------------------------------------------------------------
+
+cd $modules
+nuget install orbitalshell -Prerelease
+find . -p "*Orbital*lib*" -f -s
+find . -p "*contentFiles*" -f -s
+
+# nuget
+# -----
+
+nuget spec OrbitalShell.csproj
+nuget pack OrbitalShell.nuspec
+
+# publid OrbitalShell-cli
+# --------------------------------------------------------------
+
+cd OrbitalShell-cli
+# framework-dependent
+dotnet publish --configuration Release --output bin/publish/netcoreapp3.1/
+# framework-independent
+dotnet publish --runtime win-x64 --configuration Release --output bin/publish/win-x64/
+dotnet publish --runtime linux-x64 --configuration Release --output bin/publish/linux-x64/
+
+which dotnet
+cd "C:/Program Files/dotnet/dotnet.exe"
+
+# get nuget binaries (.netcore 3.1 assemblies)
+# --------------------------------------------------------------
+
+#...
+
