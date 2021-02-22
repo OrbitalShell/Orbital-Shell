3aefe3be179b25bcd677ee0a323372e5508fd7ed
franck gaspoz <franck.gaspoz@gmail.com>
2021-02-22 03:54:17 +0100

    update scripts

---
+++ OrbitalShell-CLI/Scripts/publish-kernel-packages.sh
@@ -0,0 +1,18 @@
+﻿#!porbsh
+# build/publish orbital shell kernel packages
+
+set version 1.0.6.nupkg
+
+cls
+echo "(b=darkgreen,f=black)                                             "
+echo "(b=darkgreen,f=black) build/publish orbital shell kernel packages (rdc)"
+echo "(b=darkgreen,f=black)                                             "
+echo
+proj
+dotnet build OrbitalShell.sln
+
+nuget-push OrbitalShell-ConsoleApp/bin/Debug/OrbitalShell-ConsoleApp.$version $key
+nuget-push OrbitalShell-Kernel/bin/Debug/OrbitalShell-Kernel.$version $key
+nuget-push OrbitalShell-Kernel-Commands/bin/Debug/OrbitalShell-Kernel-Commands.$version $key
+
+echo "(b=darkgreen,f=yellow) Done. (rdc)"
